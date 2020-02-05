4b41114e5557cf3bf55c7f602165eb492acd5bb1
dmichalczak <danielrmichalczak@gmail.com>
2020-02-05 13:24:41 +0100

    Refactoring SearchEngineExcel.py and make it return "winVersion" and "officeVersion"

--- SearchEngineExcel.py
+++ SearchEngineExcel.py
@@ -7,20 +7,21 @@ class SearchEngineExcel:
     @staticmethod
     def ScanFileForPatterns(excelPathName):
         dataSheet = ExcelReader.readExcelFile(excelPathName)
-        office = ""
-        win = ""
+        officeVersion = ""
+        winVersion = ""
         for rowNumber in range(0, dataSheet.nrows - 1):
             for cell in dataSheet.row_slice(rowNumber):
                 if str(cell.value).find('Office') != -1:
-                    office = cell.value[str(cell.value).find("Office"):str(cell.value).find("Office") + 30]
+                    officeVersion = cell.value[str(cell.value).find("Office"):str(cell.value).find("Office") + 30]
                 if str(cell.value).find('Win') != -1:
-                    win = cell.value[str(cell.value).find("Win"):str(cell.value).find("Win") + 30]
+                    winVersion = cell.value[str(cell.value).find("Win"):str(cell.value).find("Win") + 30]
                 if str(cell.value).find('W7') != -1:
-                    win = cell.value[str(cell.value).find('W7'):str(cell.value).find('W7') + 30]
+                    winVersion = cell.value[str(cell.value).find('W7'):str(cell.value).find('W7') + 30]
                 if str(cell.value).find('W8') != -1:
-                    win = cell.value[str(cell.value).find('W8'):str(cell.value).find('W8') + 30]
+                    winVersion = cell.value[str(cell.value).find('W8'):str(cell.value).find('W8') + 30]
                 if str(cell.value).find('W10') != -1:
-                    win = cell.value[str(cell.value).find('W10'):str(cell.value).find('W10') + 30]
+                    winVersion = cell.value[str(cell.value).find('W10'):str(cell.value).find('W10') + 30]
 
-        print("Office: ", office)
-        print("Win: ", win)
+        print("Office: ", officeVersion)
+        print("Win: ", winVersion)
+        return winVersion, officeVersion
