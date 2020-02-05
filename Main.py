c37b586e40852f8f3b97d5fc8147ab9f0c52afff
dmichalczak <danielrmichalczak@gmail.com>
2020-02-05 11:28:43 +0100

    Add detecting correct DKF number. Bad DKF will be in separate list to forward examination.

--- Main.py
+++ Main.py
@@ -5,9 +5,9 @@ EXCELPATHNAME = "C:/Users/dMichalczak/TestyPyKsiegowosc/DoTestow.xlsx"
 #DOCXPATHNAME = "D:/Poligon_Python/Faktura-VAT.docx"
 
 #DocReader.readDoc(DOCXPATHNAME)
-
-rowObjects = RowMaker.readExcelFileToSheetAndMakingObject(EXCELPATHNAME)
+rowObjects, rowsWithBadDKF = RowMaker.readExcelFileToSheetAndMakingObject(EXCELPATHNAME)
 rowObjects[0].showRowObject()
 rowObjects[6].showRowObject()
+print("Number of rows with bad DKF:  ", len(rowsWithBadDKF))
 
 
--- RowToObjects.py
+++ RowToObjects.py
@@ -10,9 +10,19 @@ class RowMaker:
         dataSheet = ExcelReader.readExcelFile(excelPathName)
         print("Excel read successfully")
         listOfObjects = []
+        rowsWithBadDKF = []
 
         for rowNumber in range(1, dataSheet.nrows - 1):
-            listOfObjects.append(RowRecord(dataSheet.row_slice(rowNumber)[3], dataSheet.row_slice(rowNumber)[8]))
+
+            # Check if row contain correct DKF
+            if str(dataSheet.row_slice(rowNumber)[8].value).find("+") != -1 or \
+                    dataSheet.row_slice(rowNumber)[8].value == "" or \
+                    dataSheet.row_slice(rowNumber)[8].value == "brak skanu" or \
+                    dataSheet.row_slice(rowNumber)[8].value == "FS-01WW/00032360/2014 - ZŚW 799/2014" or \
+                    dataSheet.row_slice(rowNumber)[8].value == "ID:379199":
+                rowsWithBadDKF.append(rowNumber)
+            else:
+                listOfObjects.append(RowRecord(dataSheet.row_slice(rowNumber)[3], dataSheet.row_slice(rowNumber)[8]))
 
         print("Rows as objects add successfully", " Number of readed rows: ", dataSheet.nrows - 1)
-        return listOfObjects
+        return listOfObjects, rowsWithBadDKF
